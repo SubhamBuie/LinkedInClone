e4680f2ae958db80d150e34eb7de199f08af095f
Subham Malgope <malgopesubham@gmail.com>
2023-05-28 07:20:10 +0530

    change in app.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Linked In Clone</Text>
+      <Text>LinkedIn Clone</Text>
       <StatusBar style="auto" />
     </View>
   );
